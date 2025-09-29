c2b6d9bc2bbb4b230a04f98a995ef6104bc3c37b
Tak002 <andycho1120@naver.com>
2025-09-30 00:06:23 +0900

    Add: test web page

--- websocket/src/main/java/Spring_Websocket/websocket/WebsocketConfig.java
+++ websocket/src/main/java/Spring_Websocket/websocket/WebsocketConfig.java
@@ -13,7 +13,8 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
 
     }
 
