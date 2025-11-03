57e205f25c667958ac5f4ed262f49d51a6bb1367
Tak002 <andycho1120@naver.com>
2025-11-04 00:41:26 +0900

    Feat: Update CurrentUserDto and rename UserController to UserInfoController
    - 부수 변경: CurrentUserDto에 department, bio, birthday 필드 추가
    - 부수 변경: UserController를 UserInfoController로 이름 변경

--- app-service/src/main/java/com/tak/app_service/dto/CurrentUserDto.java
+++ app-service/src/main/java/com/tak/app_service/dto/CurrentUserDto.java
@@ -2,12 +2,21 @@ package com.tak.app_service.dto;
 
 import com.tak.common.appUser.AppUser;
 
+import java.time.LocalDate;
+
 public record CurrentUserDto(
         String accessToken,
         AuthUser user
 ) {
     public record AuthUser(
-            String id, String email, String nickname, AppUser.Role role, AppUser.Status status // ACTIVE/DEACTIVATED/...
+            String id,
+            String email,
+            String nickname,
+            AppUser.Role role,
+            AppUser.Status status, // ACTIVE/DEACTIVATED/...
+            String department,
+            String bio,
+            LocalDate birthday
     ) {
         public static AuthUser fromAppUser(AppUser appUser) {
             return new AuthUser(
@@ -16,6 +25,9 @@ public record CurrentUserDto(
                     appUser.getNickname(),
                     appUser.getRole(),
                     appUser.getStatus()
+                    , appUser.getDepartment()
+                    , appUser.getBio()
+                    , appUser.getBirthDate()
             );
         }
     }
--- app-service/src/main/java/com/tak/app_service/user/UserInfoController.java
+++ app-service/src/main/java/com/tak/app_service/user/UserInfoController.java
@@ -5,20 +5,19 @@ import com.tak.common.api.ApiResponseBody;
 import com.tak.common.appUser.AppUser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/api/users")
-public class UserController {
+public class UserInfoController {
 
     //todo AccessToken 실제 유저 정보 연동
     @GetMapping("/me")
     public ResponseEntity<?> getMyProfile() {
         return ResponseEntity.ok().body(ApiResponseBody.ok(new CurrentUserDto("dummy-access",
-                new CurrentUserDto.AuthUser("1", "1234", "nickname", AppUser.Role.user, AppUser.Status.active))));
+                new CurrentUserDto.AuthUser("1", "1234", "nickname", AppUser.Role.user, AppUser.Status.active, "CS", "Hello, I'm using TAK!", LocalDate.of(2002,11,20)))));
     }
 
     //todo 유저 프로필 수정 기능 구현
@@ -26,10 +25,10 @@ public class UserController {
     public ResponseEntity<?> updateMyProfile() {
         return ResponseEntity.ok().body(ApiResponseBody.ok());
     }
-
+    
+    //todo 유저 조회 연동
     @GetMapping("/{id}")
-    public ResponseEntity<?> getUserProfile(@RequestParam Long id) {
+    public ResponseEntity<?> getUserProfile(@PathVariable String id) {
         return ResponseEntity.ok().body(ApiResponseBody.ok(new CurrentUserDto("dummy-access",
-                new CurrentUserDto.AuthUser(id.toString(), "1234", "nickname", AppUser.Role.user, AppUser.Status.active))));
-    }
+                new CurrentUserDto.AuthUser(id, "1234", "nickname", AppUser.Role.user, AppUser.Status.active, "CS", "Hello, I'm using TAK!", LocalDate.of(2002,11,20)))));    }
 }
