54d2c31158942e4fcb63e34de80b6922bd9d0252
Tak002 <andycho1120@naver.com>
2025-12-03 01:46:57 +0900

    Refactor: Rename ApiResponseBody to ApiResponse and update usages

--- app-media/src/main/java/com/tak/app_media/controller/MediaController.java
+++ app-media/src/main/java/com/tak/app_media/controller/MediaController.java
@@ -2,9 +2,8 @@ package com.tak.app_media.controller;
 
 import com.tak.app_media.dto.PresignedUrlResponse;
 import com.tak.app_media.service.MediaService;
-import com.tak.common.api.ApiResponseBody;
+import com.tak.common.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,18 +14,18 @@ public class MediaController {
 
     // input: 어디에 쓸 이미지인지, 선택적으로 userId
     @PostMapping("/upload/{type}")
-    public ResponseEntity<?> generatePresignedUploadUrl(@PathVariable String type, @RequestAttribute("userId") Long userId) {
+    public ApiResponse<?> generatePresignedUploadUrl(@PathVariable String type, @RequestAttribute("userId") Long userId) {
         PresignedUrlResponse presignedUrlResponse = mediaService.generatePresignedURL(type, userId);
-        return ResponseEntity.ok().body(ApiResponseBody.ok(presignedUrlResponse));
+        return ApiResponse.ok(presignedUrlResponse);
     }
     @GetMapping("/{mediaId}")
-    public ResponseEntity<?> getMediaById(@PathVariable Long mediaId) {
+    public ApiResponse<?> getMediaById(@PathVariable Long mediaId) {
         try{
             String mediaUrl = mediaService.getMediaUrl(mediaId);
-            return ResponseEntity.ok().body(ApiResponseBody.ok(mediaUrl));
+            return ApiResponse.ok(mediaUrl);
         }
         catch (Exception e){
-            return ResponseEntity.ok().body(ApiResponseBody.fail("MEDIA.NOT_FOUND", "존재하지 않는 mediaId"));
+            return ApiResponse.fail("MEDIA.NOT_FOUND", "존재하지 않는 mediaId");
         }
     }
 }
